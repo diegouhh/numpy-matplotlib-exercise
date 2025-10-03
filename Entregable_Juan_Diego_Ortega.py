70438b81554753da3c725eb8c38dedab0b7b1ee3
diegouhh <diego.ortega1@udea.edu.co>
2025-10-03 11:45:37 -0500

    Start Python file with imports

---
+++ Entregable_Juan_Diego_Ortega.py
@@ -0,0 +1,9 @@
+# autor: Juan Diego Ortega
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Creo el vector aleatorio
+np.random.seed(7)  # para que siempre salgan iguales
+vector = np.random.rand(720)
+
+# aquí haré el script
